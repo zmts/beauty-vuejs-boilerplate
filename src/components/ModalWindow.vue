0da71314f1399a378d6d5576baa486d675470756
Sasha Zmts <sa.zmts@gmail.com>
2017-12-16 20:36:32 +0200

    add ModalWindow component

---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,90 @@
+<template>
+  <transition name="modal">
+    <div class="modal-window overlay component" v-if="showLocal" @click="onClickOverlay" ref="overlay">
+        <div class="modal-content">
+          <div class="header">
+            <span class="close" @click="close" v-if="showCloseButton">close</span>
+          </div>
+          <slot></slot>
+        </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    name: 'ModalWindow',
+    props: {
+      show: {
+        type: Boolean,
+        default: false
+      },
+      showCloseButton: {
+        type: Boolean,
+        default: true
+      },
+      closeOnOverlay: {
+        type: Boolean,
+        default: false
+      }
+    },
+    watch: {
+      show: function (newVal) {
+        this.showLocal = newVal
+      }
+    },
+    data () {
+      return {
+        showLocal: this.show
+      }
+    },
+    methods: {
+      close () {
+        this.$emit('close')
+      },
+      onClickOverlay ($event) {
+        if (this.closeOnOverlay && $event && $event.target === this.$refs.overlay) {
+          this.close()
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .modal-window.overlay.component {
+    width: 100vw;
+    height: 100vh;
+    z-index: 100;
+    position: absolute;
+    top: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.2);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .modal-content {
+      min-width: 200px;
+      background-color: red;
+      box-shadow: 0 7px 20px rgba(0, 0, 0, 0.5);
+      .header {
+        display: flex;
+        justify-content: flex-end;
+        .close {
+          border: 1px solid black;
+          padding: 2px;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+
+  /* modal transition */
+  .modal-enter-active, .modal-leave-active {
+    transition: all .3s ease;
+  }
+  .modal-enter, .modal-leave-to {
+    padding-top: 100px;
+    opacity: 0;
+  }
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="home page">
     <h1>{{msg}}</h1>
+    <button @click="isShownModal = true">show modal</button>
+    <ModalWindow
+      closeOnOverlay
+      @close="onCloseModal"
+      :show="isShownModal">
+      <div>
+        hi here
+      </div>
+    </ModalWindow>
   </div>
 </template>
 
 <script>
+  import ModalWindow from '@/components/ModalWindow.vue'
   export default {
     name: 'IndexPage',
+    components: {
+      ModalWindow
+    },
     data () {
       return {
-        msg: 'Welcome to Index!!!'
+        msg: 'Welcome to Index!!!',
+        isShownModal: true
+      }
+    },
+
+    methods: {
+      onCloseModal () {
+        this.isShownModal = false
       }
     }
   }
