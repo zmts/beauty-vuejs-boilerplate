8fa039e43416f5c8b130ec3029d8bc0a2a2e10a4
Sasha Zmts <sa.zmts@gmail.com>
2018-01-06 18:02:47 +0200

    add TagsView component

---
+++ src/components/TagsView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="tags-view component">
+    <div class="tag" v-for="tag in tags" :key="tag.id">
+      <router-link :to="{ name: 'tag', params: { name: tag.name }}">{{ tag.label }}<span class="comma">,</span></router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TagsView',
+    props: {
+      tagsList: {
+        type: Array,
+        default: () => []
+      },
+      labelName: {
+        type: String,
+        default: 'label'
+      }
+    },
+    computed: {
+      tags () {
+        return this.tagsList.map(item => ({label: item[this.labelName], ...item}))
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .tags-view.component {
+    display: flex;
+    flex-wrap: wrap;
+    a {
+      color: inherit;
+      &:hover {
+        opacity: 0.7;
+      }
+    }
+    .tag{
+      margin-bottom: 5px;
+      margin-right: 5px;
+      &:last-child .comma {
+        display: none;
+      }
+    }
+  }
+</style>
