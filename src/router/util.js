bde1b6604ac739cf3eb6110eb09a4ddb911c5712
Sasha Zmts <sa.zmts@gmail.com>
2018-03-14 22:00:28 +0200

    minor

--- src/router/util.js
+++ src/router/util.js
@@ -5,7 +5,7 @@ export function routePropResolver (routeOptions) {
 
   Object.keys(query).forEach(key => {
     if (Number(query[key]) && +query[key]) {
-      resultQueryParams[key] = query[key]
+      resultQueryParams[key] = +query[key]
     }
     if (String(query[key]) && query[key].length && !['0', 'null', 'undefined', 'NaN'].includes(query[key])) {
       resultQueryParams[key] = query[key]
