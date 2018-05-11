7ce31449c8ff69bad61775f8ff500e31eed58764
Sasha Zmts <sa.zmts@gmail.com>
2018-05-11 11:51:17 +0300

    add UiOnClickOutside

---
+++ src/components/UiOnClickOutside.vue
@@ -0,0 +1,21 @@
+<!-- https://codesandbox.io/s/qvow6lvp64 -->
+<script>
+  export default {
+    name: 'UiOnClickOutside',
+    props: ['do'],
+    mounted () {
+      const listener = e => {
+        if (e.target !== this.$el && !this.$el.contains(e.target)) {
+          this.do()
+        }
+      }
+      document.addEventListener('click', listener)
+      this.$once('hook:destroyed', () => {
+        document.removeEventListener('click', listener)
+      })
+    },
+    render () {
+      return this.$slots.default[0]
+    }
+  }
+</script>
